f1284d3a48d0463398488fe490a01ba03e80d2d5
oMatheusmol <80729568+oMatheusmol@users.noreply.github.com>
2021-03-30 23:58:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const address = process.argv[2]
+
+if(!address)return console.log('Please provide an address')
+geocode(address, (error, data) => {
+    
+    if(error){
+        return console.log(error)
+    }    
+    // console.log('Error', error)
+    // console.log('Data', data)
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        
+        if(error){
+            return console.log(error)
+        }
+
+        console.log(data.location)
+        console.log(forecastData)
+        
+        // console.log('Error', error)
+        // console.log('Data', data)
+    })
+})
+
+
